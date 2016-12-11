d83becc747043f13aff903dc9e4b02d0f98ce83e
hpan <hao_pan@outlook.com>
2016-12-11 21:55:42 +0800

    add info links

--- src/components/AddInfoLink.js
+++ src/components/AddInfoLink.js
@@ -7,9 +7,11 @@ class AddInfoLink extends React.Component {
         return (
             <div id={this.props.id} className="addInfoLink">
                 <img src={this.props.pic} className="pic"/>
-                <h3>{this.props.title}</h3>
-                <p className="paragraph">{this.props.paragraph}</p>
-                <p className="link">{this.props.link}</p>
+                <div className="container">
+                    <h4 className="title">{this.props.title}</h4>
+                    <p className="paragraph">{this.props.paragraph}</p>
+                    <p className="link">{this.props.link}</p>
+                </div>
             </div>
         )
     }
